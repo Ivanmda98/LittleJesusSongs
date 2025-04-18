7a0397d5ea707f96ab595a49222e47dd88ff889d
Ivan Martinez <ivan18.im67@gmail.com>
2025-04-18 10:52:22 -0600

    change url to consume the backend from prod

--- little-jesus-songs/src/services/getToken.js
+++ little-jesus-songs/src/services/getToken.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 
 export const getTokenService = async () => {
-   return await axios
-   .post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials',{
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: `Basic OGM2YzI5OWYyZTQzNDBjNThmZTNlZWQ2ZGY0Yzk4MDc6YzZjNmExMzYyMzY1NDgyZGFhYzc2MDk3NDA1NGI1YTc=`
-        }
-   })
-   .then((response) => {
-    return response;
-});
+    const baseURL = import.meta.env.VITE_BACKEND_URL;
+    return await axios
+        .get(`${baseURL}/api/token`)
+        .then((response) => {
+            return response;
+        });
 }
